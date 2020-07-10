fa6b9dd4933cc90b03f6fb5cb1da1e316072c281
Luke <deandrevidal@aol.com>
2020-07-10 16:00:21 -0700

    [done] benchmark testing

--- main_test.go
+++ main_test.go
@@ -27,3 +27,12 @@ func TestTableCalculate(t *testing.T) {
 		}
 	}
 }
+func benchmarkCalculate(input int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Calculate(input)
+	}
+}
+
+func BenchmarkCalculate100(b *testing.B)         { benchmarkCalculate(100, b) }
+func BenchmarkCalculateNegative100(b *testing.B) { benchmarkCalculate(-100, b) }
+func BenchmarkCalculateNegative1(b *testing.B)   { benchmarkCalculate(-1, b) }
